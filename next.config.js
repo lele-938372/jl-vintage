6e0ef5177f4292e41085ccb7a4b2787745f9997d
lele-938372 <leandromenzel492@gmail.com>
2026-03-20 13:15:16 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['@vercel/kv'],
+  },
   images: {
     domains: ['images.unsplash.com', 'cdn.pixabay.com'],
   },
 }
+
 module.exports = nextConfig
